53d7b591aade3fdb05fdf707a91644047dbd449b
欧阳澄泓 <excalibur-faith@qq.com>
2014-10-29 19:37:43 +0800

    fisrt commit

---
+++ src/main/java/org/fightteam/leeln/core/User.java
@@ -0,0 +1,20 @@
+package org.fightteam.leeln.core;
+
+/**
+ * 用户试用累
+ */
+public class User {
+    /**
+     * ssdsds
+     */
+    private Long id;
+    private String username;
+
+    /**
+     * 查找
+     * @return 名字
+     */
+    public String find(){
+        return "";
+    }
+}
