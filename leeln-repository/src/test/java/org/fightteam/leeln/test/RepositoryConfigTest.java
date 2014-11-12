2997176ee69e722b78b8f69d7e50aeb2b8a82b8a
欧阳澄泓 <excalibur-faith@qq.com>
2014-11-12 20:28:35 +0800

    change config class

--- leeln-repository/src/test/java/org/fightteam/leeln/test/RepositoryConfigTest.java
+++ leeln-repository/src/test/java/org/fightteam/leeln/test/RepositoryConfigTest.java
@@ -1,15 +1,10 @@
 package org.fightteam.leeln.test;
 
-import org.fightteam.leeln.config.AppConfig;
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-
-import javax.sql.DataSource;
 
 /**
  * description
@@ -19,7 +14,7 @@ import javax.sql.DataSource;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class RepositoryConfigTest {
 
